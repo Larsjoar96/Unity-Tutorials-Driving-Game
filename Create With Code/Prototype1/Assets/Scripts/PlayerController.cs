d3dada22c5d6c9eb77403de685d0fc2c48c3990c
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-11-21 12:56:23 +0100

    Finished Driving Game

--- Create With Code/Prototype1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float forwardInput;
+    private float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         
     }
 
@@ -14,6 +20,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move vehicle forward
-        transform.Translate(0, 0, 1);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
